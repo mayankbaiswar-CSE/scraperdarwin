6150b009269d04bc956fa9752bf056bd8d9b3f03
Mayank Baiswar <mayank.baiswar@daffodilsw.com>
2017-11-11 17:31:28 +0530

    * cors headers

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var mkdir = require('./utils/mkdir');
 var app = express();
 
 // enable cors
-app.use(cors());
+// app.use(cors());
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -44,6 +44,26 @@ app.use('/test', users);
 
 // make directory to download image to
 mkdir();
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
