4d4899ee3a94cc950d2154de083edb83f3ac75be
Mayank Baiswar <mayank.baiswar@daffodilsw.com>
2017-11-11 17:52:33 +0530

    * cors changed

--- routes/uploader.js
+++ routes/uploader.js
@@ -3,4 +3,4 @@ var uploadController = require('../controller/uploader.js');
 
 module.exports = router;
 
-router.post('/', uploadController.upload);
+// router.post('/', uploadController.upload);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var router = require('express').Router();
 var userController = require('../controller/users');
-const cors = require('cors');
 
-router.use(cors());
+router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 router.get('/:name', userController.get);
 router.get('/show/:name', userController.serve);
 router.get('/tags/all', userController.tags);
