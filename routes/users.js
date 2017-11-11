5d68bc6f65282806ee205f834f7de7f14b8828e5
Mayank Baiswar <mayank.baiswar@daffodilsw.com>
2017-11-11 18:00:07 +0530

    * origin add in cors

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var userController = require('../controller/users');
 
 router.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
